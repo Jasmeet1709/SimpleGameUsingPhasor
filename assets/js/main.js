fa7fd276845fd027e15f174827a4bf612b8c9474
Jasmeet1709 <jsmtbagga45@gmail.com>
2017-12-20 23:25:57 +0530

    assests are added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
 var game;
 
-game = new Phaser.Game(600,450,Phaser.AUTO,'');
+game = new Phaser.Game(600,600,Phaser.AUTO,'');
 
 game.state.add('Menu',Menu);
 
-game.state.add('Game',Game);
 
 game.state.start('Menu');
 
-game.state.add('Game_Over',Game_Over);
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,14 +1,45 @@
+var platforms;
+var player;
 var Menu = {
 
   preload: function(){
-    game.load.image('menu','./assets/images/menu.png');
+    game.load.image('sky','./assets/images/sky.png');
+    game.load.image('ground','./assets/images/platform.png');
+    game.load.image('star','./assets/images/star.png');
+    game.load.spritesheet('dude','./assets/images/dude.png',32,48);
   },
 
   create: function(){
-    this.add.button(0,0,'menu',this.startGame,this);
+  	game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.add.sprite(0,0,'sky');
+
+    platforms = game.add.group();
+
+    platforms.enableBody = true;
+
+    var ground = platforms.create(0,game.world.height - 64,'ground');
+    ground.scale.setTo(2,2);
+    ground.body.immovable = true;
+
+    var ledge = platforms.create(400,400,'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(-150,250,'ground');
+    ledge.body.immovable = true;
+
+    //player
+    player = game.add.sprite(32,game.world.height - 150,'dude');
+    game.physics.arcade.enable(player);
+
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    player.animations.add('left',[0,1,2,3],10,true);
+    player.animations.add('right',[5,6,7,8],10,true);
+
   },
 
-  startGame: function(){
-    this.state.start('Game');
-  }
+  
 };
