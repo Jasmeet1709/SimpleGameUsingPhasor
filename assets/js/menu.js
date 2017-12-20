93b45f3e52574973d8a7cdaea2cb55cf60e316ce
Jasmeet1709 <jsmtbagga45@gmail.com>
2017-12-20 23:56:15 +0530

    player movement is added

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,6 @@
 var platforms;
 var player;
+var cursors;
 var Menu = {
 
   preload: function(){
@@ -39,9 +40,31 @@ var Menu = {
     player.animations.add('left',[0,1,2,3],10,true);
     player.animations.add('right',[5,6,7,8],10,true);
 
+    cursors = game.input.keyboard.createCursorKeys();
+
   },
 
   update: function(){
     var hitPlatform = game.physics.arcade.collide(player,platforms);
+
+    player.body.velocity.x = 0;
+
+    if(cursors.left.isDown){
+        player.body.velocity.x = -150;
+        player.animations.play('left');
+
+    }
+    else if(cursors.right.isDown){
+        player.body.velocity.x = 150;
+        player.animations.play('right');
+    }
+    else{
+        player.animations.stop();
+        player.frame = 4;
+    }
+
+    if(cursors.up.isDown && player.body.touching.down && hitPlatform){
+        player.body.velocity.y = -350;
+    }
   }
 };
